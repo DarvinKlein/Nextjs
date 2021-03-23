edb1b9d614a5d9b23cdae76f34ad7fcc158190af
Darvin Klein Momo <darvinkleinm@gmail.com>
2021-03-22 23:27:58 -0700

    Second commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus id esse nam cupiditate provident, obcaecati earum in quaerat perferendis assumenda distinctio tempora explicabo recusandae iusto quam placeat deserunt exercitationem impedit!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas iure dicta cum modi at recusandae veniam, nulla iusto natus vero quo sit eveniet impedit obcaecati necessitatibus? Soluta beatae officia repellendus!</p>
+        </div>
+      );
+}
+ 
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>Hello Senior! Mayng gabie diha</h1>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam rem libero atque dignissimos ratione, doloremque eius id, quas laudantium accusantium quo, temporibus facilis totam odit voluptatum vero ipsa aut beatae!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam rem libero atque dignissimos ratione, doloremque eius id, quas laudantium accusantium quo, temporibus facilis totam odit voluptatum vero ipsa aut beatae!</p>
     </div>
   )
 }
