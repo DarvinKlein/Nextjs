9eb58bc026b3af09273cd6150d884436e457dbe4
DarvinKlein <darvinkleinm@gmail.com>
2021-03-28 15:42:03 -0700

    Fourth commit(Layout Component)

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam rem libero atque dignissimos ratione, doloremque eius id, quas laudantium accusantium quo, temporibus facilis totam odit voluptatum vero ipsa aut beatae!</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam rem libero atque dignissimos ratione, doloremque eius id, quas laudantium accusantium quo, temporibus facilis totam odit voluptatum vero ipsa aut beatae!</p>
       <Link href="ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
